cf14cca7de231c05084f86779e33252b4ca31e86
kaj <hi@kajetansuchanski.pl>
2024-01-01 14:36:39 +0100

    Logging out web client added & login response DTO updated

--- src/main/java/com/kwezal/bearinmind/core/auth/dto/LoginResponseDto.java
+++ src/main/java/com/kwezal/bearinmind/core/auth/dto/LoginResponseDto.java
@@ -2,4 +2,4 @@ package com.kwezal.bearinmind.core.auth.dto;
 
 import java.util.Set;
 
-public record LoginResponseDto(Long userId, Set<String> authorities) {}
+public record LoginResponseDto(Long userId, String userFullName, String userImage, Set<String> authorities) {}
